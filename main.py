24d2e8820c03354cddc9d05df9e4211c349e3620
akuldevali <akulg005@gmail.com>
2025-06-17 19:58:00 -0400

    added describe_tables_tool for extra context to the LLM and also defined Classes for arguments within the tool

--- main.py
+++ main.py
@@ -2,16 +2,25 @@ from langchain_community.chat_models import ChatOpenAI
 from langchain.prompts import ( ChatPromptTemplate,HumanMessagePromptTemplate, MessagesPlaceholder)
 from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
 from dotenv import load_dotenv
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from langchain.schema import SystemMessage
 
 load_dotenv()
 chat= ChatOpenAI()
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
+tables = list_tables()
+# print(tables)
+
 
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(content=(
+            "You are an AI agent that has access to a SQLite database.\n"
+            f"The database has tables of: {tables}\n"
+            "DO not manke any assumptions about what tables or columns exist. Instead use the 'desscribe_tables' function"
+        )),
         HumanMessagePromptTemplate.from_template("{input}"),
-        MessagesPlaceholder(variable_name="agent_scratchpad")
+        MessagesPlaceholder(variable_name="agent_scratchpad") #this is like a memory for messages
     ]
 )
 
@@ -27,4 +36,4 @@ agent_executor = AgentExecutor(
     tools=tools
 )
 
-agent_executor("How many users are in the database?")
+agent_executor("How many users have provided a shipping address?")
