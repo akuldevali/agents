adda497abe3baae4d62d69bd68d8ffe5c4278387
akuldevali <akulg005@gmail.com>
2025-06-18 14:55:59 -0400

    Custom handler for chat model

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
 from langchain.schema import SystemMessage
 from langchain.memory import ConversationBufferMemory
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
-chat= ChatOpenAI()
+
+handler = ChatModelStartHandler()
+chat= ChatOpenAI(
+    callbacks=[handler]
+)
 memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 tables = list_tables()
@@ -20,7 +25,7 @@ prompt = ChatPromptTemplate(
         SystemMessage(content=(
             "You are an AI agent that has access to a SQLite database.\n"
             f"The database has tables of: {tables}\n"
-            "DO not manke any assumptions about what tables or columns exist. Instead use the 'desscribe_tables' function"
+            "DO NOT make any assumptions about what tables or columns exist. Instead use the 'desscribe_tables' function"
         )),
         MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
