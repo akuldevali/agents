a595b0fe62365b3cdf03881398390a1699307b08
akuldevali <akulg005@gmail.com>
2025-06-18 02:25:55 -0400

    Testing agent executor memory with Converstation buffer memory

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
 from langchain.schema import SystemMessage
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 chat= ChatOpenAI()
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 tables = list_tables()
 # print(tables)
@@ -20,6 +22,7 @@ prompt = ChatPromptTemplate(
             f"The database has tables of: {tables}\n"
             "DO not manke any assumptions about what tables or columns exist. Instead use the 'desscribe_tables' function"
         )),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad") #this is like a memory for messages
     ]
@@ -34,8 +37,11 @@ agent = OpenAIFunctionsAgent(
 agent_executor = AgentExecutor(
     agent=agent,
     verbose=True,
-    tools=tools
+    tools=tools,
+    memory=memory
 )
 
-agent_executor("Who are the top 5 users with most orders. Write the results to a report file.")
+# agent_executor("Who are the top 5 users with most orders. Write the results to a report file.")
 # agent_executor("How many users have provided a shipping address?")
+agent_executor("How many orders are there?")
+agent_executor("and tell me how many of them are after 2024")
